4445b69ce9515e3ccdeae9bf78649cf249193dc0
Fisher <1186907891@qq.com>
2024-03-14 16:28:17 +0800

    add: add some concurrency course codes.

--- introduction/os-model/hello.py
+++ introduction/os-model/hello.py
@@ -1,7 +1,10 @@
 def main():
     x = 0
     for _ in range(10):
+        # b = yield 0 or b = yield 1
         b = sys_read()
         x = x * 2 + b
 
+    # left-padded with zeros 
+    # to meet the width of 10 characters specified by 010.
     sys_write(f'x = {x:010b}b')
