18bf113d6b42b4869fd47d2d2f6b72ab208fed5a
Yjsmall <1186907891@qq.com>
2024-03-16 11:13:40 +0800

    update: formate files.

--- concurrency/thread-lib/thread.h
+++ concurrency/thread-lib/thread.h
@@ -14,21 +14,20 @@ enum {
 };
 
 struct thread {
-    int id;  // Thread number: 1, 2, ...
-    int status;  // Thread status: FREE/LIVE/DEAD
-    pthread_t thread;  // Thread struct
-    void (*entry)(int);  // Entry point
+    int       id;       // Thread number: 1, 2, ...
+    int       status;   // Thread status: FREE/LIVE/DEAD
+    pthread_t thread;   // Thread struct
+    void (*entry)(int); // Entry point
 };
 
 static struct thread threads_[4096];
-static int n_ = 0;
+static int           n_ = 0;
 
 // This is the entry for a created POSIX thread. It "wraps"
 // the function call of entry(id) to be compatible to the
 // pthread library's requirements: a thread takes a void *
 // pointer as argument, and returns a pointer.
-static inline
-void *wrapper_(void *arg) {
+static inline void *wrapper_(void *arg) {
     struct thread *t = (struct thread *)arg;
     t->entry(t->id);
     return NULL;
@@ -36,28 +35,26 @@ void *wrapper_(void *arg) {
 
 // Create a thread that calls function fn. fn takes an integer
 // thread id as input argument.
-static inline
-void create(void *fn) {
+static inline void create(void *fn) {
     assert(n_ < LENGTH(threads_));
 
     // Yes, we have resource leak here!
-    threads_[n_] = (struct thread) {
-        .id = n_ + 1,
+    threads_[n_] = (struct thread){
+        .id     = n_ + 1,
         .status = T_LIVE,
-        .entry = fn,
+        .entry  = fn,
     };
     pthread_create(
-        &(threads_[n_].thread),  // a pthread_t
-        NULL,  // options; all to default
-        wrapper_,  // the wrapper function
-        &threads_[n_] // the argument to the wrapper
+        &(threads_[n_].thread), // a pthread_t
+        NULL,                   // options; all to default
+        wrapper_,               // the wrapper function
+        &threads_[n_]           // the argument to the wrapper
     );
     n_++;
 }
 
 // Wait until all threads return.
-static inline
-void join() {
+static inline void join() {
     for (int i = 0; i < LENGTH(threads_); i++) {
         struct thread *t = &threads_[i];
         if (t->status == T_LIVE) {
@@ -68,7 +65,6 @@ void join() {
 }
 
 // Join all threads when main() returns.
-__attribute__((destructor)) 
-static void cleanup() {
+__attribute__((destructor)) static void cleanup() {
     join();
 }
